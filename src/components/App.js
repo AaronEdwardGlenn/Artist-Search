9d1806c09ffd9aadf4e1e611881771eaa91bcd13
Nick <nickbrimmer@gmail.com>
2020-02-06 14:52:39 -0800

    appjs component path structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,16 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Router>
+      <Route path="/artistSearch/:search" component={} />
+      <Route path="/artistSearch/artist/:artist" component={} />
+      <Route path="/artistSearch/artist/album/:album" component={} />
+      <Route path="/artistSearch/artist/album/lyrics/:lyrics" component={} />
+    </Router>
+  );
 }
-  
