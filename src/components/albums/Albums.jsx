8cfdaf0086160ee75c17dd48829661c4daf576c1
Nick <nickbrimmer@gmail.com>
2020-02-07 12:16:15 -0800

    progress baby

--- src/components/albums/Albums.jsx
+++ src/components/albums/Albums.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import useGetAlbums from '../hooks/albums';
+import { useGetAlbums } from '../hooks/albums';
 
-const Albums = ({ match }) => {
-  console.log(match);
-  
+const Albums = ({ match }) => {  
   const artistId = match.params.artist;
 
-  const { albums } = useGetAlbums(artistId);
+  const albums = useGetAlbums(artistId);  
 
   const albumList = albums.map(album => {
-    <li key={album.id} >
-      <Link to={`/artistSearch/artist/album/${album.title}`}>
-        <img src={album.cover} />
-        <p>{album.title}</p>
-        <p>{album.date}</p>
-      </Link>
-    </li>;
+    return (
+      <li key={album.id} >
+        <Link to={`/artistSearch/artist/album/${album.title}`}>
+          <img src={`http://coverartarchive.org/release/${album.id}/front`} />
+          <p>{album.title}</p>
+          <p>{album.date}</p>
+        </Link>
+      </li>
+    );
   });
+
+
   return (
     <ul>
       {albumList}
@@ -29,11 +31,9 @@ const Albums = ({ match }) => {
 Albums.propTypes = {
   match: PropTypes.shape({
     params: PropTypes.shape({
-      artist: PropTypes.shape({
-        id: PropTypes.string.isRequired
-      })
+      artist: PropTypes.string.isRequired
     })
-  }),
+  })
 };
 
 export default Albums;
--- src/components/hooks/albums.js
+++ src/components/hooks/albums.js
@@ -3,20 +3,19 @@ import { getAlbums } from '../../services/musicBrainsAPI';
 import { getAlbumCover } from '../../services/coverArtAPI';
 
 
-export const useGetAlbums = (artistId) => {
-  const [albums, setAlbums] = useState(['Calvin Coolidge']);
+export const useGetAlbums = artistId => {
+  const [albums, setAlbums] = useState([{}]);
 
   useEffect(() => {
     getAlbums(artistId)
-      .then(setAlbums)
-      .then(albums => {
-        albums.forEach(album => {
-          album.cover = getAlbumCover(album.id);
-        });
-      });
-
-
+      .then(setAlbums);
   }, [artistId]);
 
-  return { albums };
+  return albums ;
 };
+
+export const useGetArtwork = id => {
+  const [artwork, setArtwork] = useState('');
+
+  useEffect
+}
